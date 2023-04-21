c1f949f178d3f6fb252f2db3aae0dee414636585
Juan Jose Curutchet <juanjo.curutchet78@gmail.com>
2023-04-21 13:51:35 -0300

    agrego uml

--- src/tragamonedasFrutas.ts
+++ src/tragamonedasFrutas.ts
@@ -2,18 +2,18 @@ import { Frutas } from "./frutas";
 import { Jugador } from "./jugador";
 import { Tragamonedas } from "./tragamonedas";
 
-export class TragamonedasFrutas extends Tragamonedas{
+export class TragamonedasFrutas extends Tragamonedas {
     private guia: Frutas[];
     private tirada: number[][];
 
 
-    public constructor (pJugador:Jugador,pNombre:string){
-        super(pJugador,pNombre);
-        this.guia= new Array ();
-        this.tirada= [];
+    public constructor(pJugador: Jugador, pNombre: string) {
+        super(pJugador, pNombre);
+        this.guia = new Array();
+        this.tirada = [];
     }
 
-    public reglamentoJuego():string[]{
+    public reglamentoJuego(): string[] {
         const reglamento: string[] = new Array;
         reglamento.push(`El juego consiste en conseguir tres frutas iguales`);
         reglamento.push(`Cada guia tiene quince frutas`);
@@ -23,26 +23,26 @@ export class TragamonedasFrutas extends Tragamonedas{
         reglamento.push(`si salen dispersas en distintas guias obtendras cinco veces la apuesta.\n`);
         return reglamento;
     }
-    public cargarGuia():void{
-        let fruta0= new Frutas(" frutilla");
-        let fruta1= new Frutas("  banana ");
-        let fruta2= new Frutas("  manzana ");
-        let fruta3= new Frutas("   pera   ");
-        let fruta4= new Frutas("   mango ");
-        let fruta5= new Frutas(" arandano ");
-        let fruta6= new Frutas("  cereza ");
-        let fruta7= new Frutas("    uva  ");
-        let fruta8= new Frutas("   kiwi ");
-        let fruta9= new Frutas("  naranja ");
-        let fruta10= new Frutas(" mandarina");
-        let fruta11= new Frutas("  sandia ");
-        let fruta12= new Frutas("   melon ");
-        let fruta13= new Frutas("   caqui ");
-        let fruta14= new Frutas("   anana ");
-        this.guia.push(fruta0,fruta1,fruta2,fruta3,fruta4,fruta5,fruta6,fruta7,fruta8,fruta9,fruta10,fruta11,fruta12,fruta13,fruta14);
+    public cargarGuia(): void {
+        let fruta0 = new Frutas(" frutilla");
+        let fruta1 = new Frutas("  banana ");
+        let fruta2 = new Frutas("  manzana ");
+        let fruta3 = new Frutas("   pera   ");
+        let fruta4 = new Frutas("   mango ");
+        let fruta5 = new Frutas(" arandano ");
+        let fruta6 = new Frutas("  cereza ");
+        let fruta7 = new Frutas("    uva  ");
+        let fruta8 = new Frutas("   kiwi ");
+        let fruta9 = new Frutas("  naranja ");
+        let fruta10 = new Frutas(" mandarina");
+        let fruta11 = new Frutas("  sandia ");
+        let fruta12 = new Frutas("   melon ");
+        let fruta13 = new Frutas("   caqui ");
+        let fruta14 = new Frutas("   anana ");
+        this.guia.push(fruta0, fruta1, fruta2, fruta3, fruta4, fruta5, fruta6, fruta7, fruta8, fruta9, fruta10, fruta11, fruta12, fruta13, fruta14);
     }
 
-    public setTirada():void{
+    /*public setTirada():void{
         this.tirada=[];
         let indice : number;
         for (let i:number=0; i<3; i++){
@@ -59,20 +59,27 @@ export class TragamonedasFrutas extends Tragamonedas{
         }
         
         
+    }*/
+    public setTirada(): void {
+        this.tirada = [
+            [1, 5, 3],
+            [2, 5, 8],
+            [5, 1, 3],
+        ];
     }
 
-    public mostrarEnPantalla():string[]{
-        let aux:string[] = new Array;
-        for (let i:number=0;i<this.tirada.length;i++){
-            for (let j:number=0;j<this.tirada[i].length;j++){
+    public mostrarEnPantalla(): string[] {
+        let aux: string[] = new Array;
+        for (let i: number = 0; i < this.tirada.length; i++) {
+            for (let j: number = 0; j < this.tirada[i].length; j++) {
                 aux.push(`${this.guia[this.tirada[i][j]].getNombre()}`);
             }
         }
         return aux;
     }
 
-    private verificarTresIguales():boolean{
-        let condicion:boolean=false;
+    private verificarTresIguales(): boolean {
+        /*let condicion:boolean=false;
         let cantidad:number;
         for (let j:number =0;j<3;j++){
             cantidad=1;
@@ -86,41 +93,61 @@ export class TragamonedasFrutas extends Tragamonedas{
             }
         }
 
-        return condicion;
-    }
-
-    private verificarLinea():boolean{
-        let condicion:boolean=false;
-        for (let i:number =0;i<3;i++){
-            if ((this.tirada[i][1]===this.tirada[i][0])&&(this.tirada[i][2]===this.tirada[i][0])){
-                condicion=true;
+        return condicion;*/
+        // Comprobar filas
+        for (let i = 0; i < 3; i++) {
+            if (this.tirada[0][i] === this.tirada[1][i] && this.tirada[0][i] === this.tirada[2][i]) {
+                return true;
             }
         }
-        return condicion;
+
+        // Si no se encontraron tres iguales en cada array
+        return false;
     }
 
-    private verificarDiagonal():boolean{
-        let condicion:boolean=false;
-        if ((this.tirada[0][0]===this.tirada[1][1])&&(this.tirada[2][2]===this.tirada[1][1])){
-            condicion=true;
-        } else if ((this.tirada[0][2]===this.tirada[1][1])&&(this.tirada[2][0]===this.tirada[1][1])){
-            condicion=true;
+
+
+
+
+
+
+
+
+
+
+
+    private verificarLinea(): boolean {
+    let condicion: boolean = false;
+    for (let i: number = 0; i < 3; i++) {
+        if ((this.tirada[i][1] === this.tirada[i][0]) && (this.tirada[i][2] === this.tirada[i][0])) {
+            condicion = true;
         }
-        return condicion;
     }
-
-    public calcularPremio():number{
-        let premio:number=0;
-        if (this.verificarTresIguales()===true){                             //Verifica que haya tres frutas iguales. Si hay tres iguales entrega 5 veces la apuesta
-            premio=this.jugador.getApuesta()*5;
-        } 
-        if (this.verificarLinea()===true){                                     //Verifica que haya tres frutas iguales en linea. Si hay tres en linea entrega 10 veces la apuesta
-            premio=premio*2;
-        } else if (this.verificarDiagonal()===true){                                     //Verifica que haya tres frutas iguales en diagonal. Si hay tres iguales entrega 15 veces la apuesta
-            premio=premio*3;
-        }
-        return premio;                                                                                          // retorna el premio, si todas fallaron es 0;
-    }
-
-    
+    return condicion;
 }
+
+    private verificarDiagonal(): boolean {
+    let condicion: boolean = false;
+    if ((this.tirada[0][0] === this.tirada[1][1]) && (this.tirada[2][2] === this.tirada[1][1])) {
+        condicion = true;
+    } else if ((this.tirada[0][2] === this.tirada[1][1]) && (this.tirada[2][0] === this.tirada[1][1])) {
+        condicion = true;
+    }
+    return condicion;
+}
+
+    public calcularPremio(): number {
+    let premio: number = 0;
+    if (this.verificarTresIguales() === true) {                             //Verifica que haya tres frutas iguales. Si hay tres iguales entrega 5 veces la apuesta
+        premio = this.jugador.getApuesta() * 5;
+    }
+    if (this.verificarLinea() === true) {                                     //Verifica que haya tres frutas iguales en linea. Si hay tres en linea entrega 10 veces la apuesta
+        premio = premio * 2;
+    } else if (this.verificarDiagonal() === true) {                                     //Verifica que haya tres frutas iguales en diagonal. Si hay tres iguales entrega 15 veces la apuesta
+        premio = premio * 3;
+    }
+    return premio;                                                                                          // retorna el premio, si todas fallaron es 0;
+}
+}
+
+
